0b450a9bb6ab0348b33cb9be6772b5c778622eeb
Robin Bron <finwo@pm.me>
2019-10-11 11:51:49 +0200

    Patched RelationType:semanticRelation; Added RelationType:mappingRelation

--- src/OpenSkos/RelationType/Controller/RelationType.php
+++ src/OpenSkos/RelationType/Controller/RelationType.php
@@ -56,30 +56,80 @@ final class RelationType
         $openskos->setType('owl:Ontology');
         $openskos->addLiteral('dc:title', 'OpenSkos RelationType vocabulary');
 
+        // Semantic Relation
         $semanticRelation = $graph->resource('openskos:semanticRelation');
-        $semanticRelation->setType('owl:ObjectProperty');
+        $semanticRelation->setType('rdf:Property');
+        $semanticRelation->addResource('rdf:type', 'owl:ObjectProperty');
         $semanticRelation->addLiteral('rdfs:domain', 'openskos:Concept');
         $semanticRelation->addLiteral('rdfs:range', 'openskos:Concept');
 
         $related = $graph->resource('openskos:related');
-        $related->setType('owl:SymmetricProperty');
+        $related->setType('rdf:Property');
+        $related->addResource('rdf:type', 'owl:ObjectProperty');
+        $related->addResource('rdf:type', 'owl:SymmetricProperty');
         $related->addResource('rdfs:subPropertyOf', $semanticRelation);
 
         $broaderTransitive = $graph->resource('openskos:broaderTransitive');
-        $broaderTransitive->setType('owl:TransitiveProperty');
+        $broaderTransitive->setType('rdf:Property');
+        $broaderTransitive->addResource('rdf:type', 'owl:ObjectProperty');
+        $broaderTransitive->addResource('rdf:type', 'owl:TransitiveProperty');
         $broaderTransitive->addResource('rdfs:subPropertyOf', $semanticRelation);
 
         $narrowerTransitive = $graph->resource('openskos:narrowerTransitive');
-        $narrowerTransitive->setType('owl:TransitiveProperty');
+        $narrowerTransitive->setType('rdf:Property');
+        $narrowerTransitive->addResource('rdf:type', 'owl:ObjectProperty');
+        $narrowerTransitive->addResource('rdf:type', 'owl:TransitiveProperty');
         $narrowerTransitive->addResource('rdfs:subPropertyOf', $semanticRelation);
 
         $broader = $graph->resource('openskos:broader');
-        $broader->setType('owl:ObjectProperty');
+        $broader->setType('rdf:Property');
+        $broader->addResource('rdf:type', 'owl:ObjectProperty');
         $broader->addResource('rdfs:subPropertyOf', $broaderTransitive);
 
         $narrower = $graph->resource('openskos:narrower');
-        $narrower->setType('owl:ObjectProperty');
+        $narrower->setType('rdf:Property');
+        $narrower->addResource('rdf:type', 'owl:ObjectProperty');
         $narrower->addResource('rdfs:subPropertyOf', $narrowerTransitive);
+
+        //////////////////////////////////////////////////////////
+
+        // Mapping relation
+        $mappingRelation = $graph->resource('openskos:mappingRelation');
+        $mappingRelation->setType('rdf:Property');
+        $mappingRelation->addResource('rdf:type', 'owl:ObjectProperty');
+        $mappingRelation->addResource('rdfs:subPropertyOf', $semanticRelation);
+
+        $closeMatch = $graph->resource('openskos:closeMatch');
+        $closeMatch->setType('rdf:Property');
+        $closeMatch->addResource('rdf:type', 'owl:ObjectProperty');
+        $closeMatch->addResource('rdf:type', 'owl:SymmetricProperty');
+        $closeMatch->addResource('rdfs:subPropertyOf', $mappingRelation);
+
+        $exactMatch = $graph->resource('openskos:exactMatch');
+        $exactMatch->setType('rdf:Property');
+        $exactMatch->addResource('rdf:type', 'owl:ObjectProperty');
+        $exactMatch->addResource('rdf:type', 'owl:SymmetricProperty');
+        $exactMatch->addResource('rdf:type', 'owl:TransitiveProperty');
+        $exactMatch->addResource('rdfs:subPropertyOf', $closeMatch);
+
+        $broadMatch = $graph->resource('openskos:broadMatch');
+        $broadMatch->setType('rdf:Property');
+        $broadMatch->addResource('rdf:type', 'owl:ObjectProperty');
+        $broadMatch->addResource('rdfs:subPropertyOf', $mappingRelation);
+        $broadMatch->addResource('rdfs:subPropertyOf', $broader);
+
+        $narrowMatch = $graph->resource('openskos:narrowMatch');
+        $narrowMatch->setType('rdf:Property');
+        $narrowMatch->addResource('rdf:type', 'owl:ObjectProperty');
+        $narrowMatch->addResource('rdfs:subPropertyOf', $mappingRelation);
+        $narrowMatch->addResource('rdfs:subPropertyOf', $narrower);
+
+        $relatedMatch = $graph->resource('openskos:relatedMatch');
+        $relatedMatch->setType('rdf:Property');
+        $relatedMatch->addResource('rdf:type', 'owl:ObjectProperty');
+        $relatedMatch->addResource('rdf:type', 'owl:SymmetricProperty');
+        $relatedMatch->addResource('rdfs:subPropertyOf', $mappingRelation);
+        $relatedMatch->addResource('rdfs:subPropertyOf', $related);
 
         return new DirectGraphResponse(
           $graph,
