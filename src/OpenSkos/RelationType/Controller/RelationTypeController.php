8bb429159ec3b092e2a563c49c66666024bf9c9f
Robin Bron <finwo@pm.me>
2019-10-14 12:39:52 +0200

    Added /relationtype/{id} endpoint

--- src/OpenSkos/RelationType/Controller/RelationTypeController.php
+++ src/OpenSkos/RelationType/Controller/RelationTypeController.php
@@ -43,4 +43,48 @@ final class RelationTypeController
             $apiRequest->getFormat()
         );
     }
+
+    /**
+     * @Route(path="/relationtype/{id}", methods={"GET"})
+     *
+     * @param string     $id
+     * @param ApiRequest $apiRequest
+     *
+     * @return DirectGraphResponse
+     */
+    public function getRelationType(
+        string $id,
+        ApiRequest $apiRequest
+    ): DirectGraphResponse {
+
+        // Build source data
+        $graph        = RelationType::relationTypes();
+        $relationtype = null;
+        $options      = [
+            $graph->resource($id),
+            $graph->resource('openskos:'.$id),
+        ];
+
+        // Simple 'exists' filter
+        foreach($options as $resource) {
+            if (is_null($resource->type())) continue;
+            $relationtype = $resource;
+        }
+
+        // TODO: Handle 404 in a nicer way than an empty graph
+        $resources = $graph->resources();
+        foreach($resources as $key => $resource) {
+            if (!is_null($relationtype)) {
+                if ($graph->resource($key)->getUri() === $relationtype->getUri()) continue;
+            }
+            foreach($resource->properties() as $property) {
+                $graph->delete($resource, $property, null);
+            }
+        }
+
+        return new DirectGraphResponse(
+            $graph,
+            $apiRequest->getFormat()
+        );
+    }
 }
