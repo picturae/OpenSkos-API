511bcfb05f6ac298d4fe188e0136d84993191db5
Robin Bron <finwo@pm.me>
2019-10-21 13:46:55 +0200

    Added status, version, license and copyright to /ping

--- src/Healthcheck/PingController.php
+++ src/Healthcheck/PingController.php
@@ -7,14 +7,52 @@ namespace App\Healthcheck;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 final class PingController extends AbstractController
 {
     /**
-     * @Route(path="/ping", methods={"GET"})
+     * @var KernelInterface
+     */
+    private $appKernel;
+
+    /**
+     * @param KernelInterface $appKernel
+     */
+    public function __construct(
+        KernelInterface $appKernel = null
+    ) {
+        if (!is_null($appKernel)) {
+            $this->appKernel = $appKernel;
+        }
+    }
+
+    /**
+     * @Route(path="/ping.{format?}", methods={"GET"})
+     *
+     * @param ApiRequest $apiRequest
+     *
+     * @return Response
      */
     public function ping(): Response
     {
-        return new Response('Hello OpenSkos world!');
+        // Fetch composer data for the version
+        $projectDir = $this->appKernel->getProjectDir();
+        $composer = json_decode(file_get_contents(
+            $projectDir.DIRECTORY_SEPARATOR.'composer.json'
+        ));
+
+        return new Response(json_encode([
+            'status' => 'ok',
+            'version' => $composer->version ?? 'n/a',
+            'license' => $composer->license ?? 'proprietary',
+            'copyright' => [
+                'holder' => 'Picturae',
+                'published' => 2019,
+                'revised' => 2019,
+            ],
+        ]), 200, [
+            'Content-Type' => 'application/json',
+        ]);
     }
 }
