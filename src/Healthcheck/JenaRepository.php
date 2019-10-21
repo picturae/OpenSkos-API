0f7325a1146c62cd9ac0aa9830b57d90b393346d
Robin Bron <finwo@pm.me>
2019-10-21 14:36:24 +0200

    Added actual healthcheck in /ping

---
+++ src/Healthcheck/JenaRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Healthcheck;
+
+use App\Ontology\Foaf;
+use App\Repository\AbstractRepository;
+
+final class JenaRepository extends AbstractRepository
+{
+    const DOCUMENT_CLASS = Person::class;
+    const DOCUMENT_TYPE = Foaf::PERSON;
+}
--- src/Healthcheck/PingController.php
+++ src/Healthcheck/PingController.php
@@ -30,20 +30,68 @@ final class PingController extends AbstractController
     /**
      * @Route(path="/ping.{format?}", methods={"GET"})
      *
-     * @param ApiRequest $apiRequest
+     * @param JenaRepository $jenaRepository
      *
      * @return Response
      */
-    public function ping(): Response
-    {
+    public function ping(
+        JenaRepository $jenaRepository
+    ): Response {
         // Fetch composer data for the version
         $projectDir = $this->appKernel->getProjectDir();
         $composer = json_decode(file_get_contents(
             $projectDir.DIRECTORY_SEPARATOR.'composer.json'
         ));
 
+        // Define status checks
+        // TODO: define these elsewhere
+        /** @var callable[] $statusChecks */
+        $statusChecks = [
+            // Check if jena is up & usable
+            function () use ($jenaRepository): bool {
+                try {
+                    $persons = $jenaRepository->all(0, 1);
+
+                    return true;
+                } catch (\Exception $e) {
+                    return false;
+                }
+            },
+
+            // At least 1 user must exist in jena
+            function () use ($jenaRepository): bool {
+                try {
+                    $persons = $jenaRepository->all(0, 1);
+
+                    return 1 === count($persons);
+                } catch (\Exception $e) {
+                    return false;
+                }
+            },
+        ];
+
+        // Run all status checks
+        $pass = 0;
+        $fail = 0;
+        foreach ($statusChecks as $statusCheck) {
+            if (call_user_func($statusCheck)) {
+                ++$pass;
+            } else {
+                ++$fail;
+            }
+        }
+
+        // Define status
+        if (0 === $fail) {
+            $status = 'ok';
+        } elseif (0 === $pass) {
+            $status = 'down';
+        } else {
+            $status = 'partial';
+        }
+
         return new Response(json_encode([
-            'status' => 'ok',
+            'status' => $status,
             'version' => $composer->version ?? 'n/a',
             'license' => $composer->license ?? 'proprietary',
             'copyright' => [
