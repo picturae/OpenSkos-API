530b9d86e49c04da05f77396f69b6d5f61b3ecab
Robin Bron <finwo@pm.me>
2019-11-12 16:57:23 +0100

    Use the right document type in SetRepository

--- src/OpenSkos/Set/SetRepository.php
+++ src/OpenSkos/Set/SetRepository.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace App\OpenSkos\Set;
 
-use App\Ontology\Org;
+use App\Ontology\OpenSkos;
 use App\Repository\AbstractRepository;
 
 final class SetRepository extends AbstractRepository
 {
     const DOCUMENT_CLASS = Set::class;
-    const DOCUMENT_TYPE = Org::FORMAL_ORGANIZATION;
+    const DOCUMENT_TYPE = OpenSkos::SET;
 }
