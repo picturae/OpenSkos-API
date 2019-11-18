a08f13365c10d84492bf8197f6c3b7240bb3957b
Robin Bron <finwo@pm.me>
2019-11-18 16:57:32 +0100

    Updated install-test-env with easier version selection

--- integrationtestsettings/install-test-env.sh
+++ integrationtestsettings/install-test-env.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
+SOLR_VERSION=7.5.0
+JENA_VERSION=2.6.0
 
 #install apache2
 
 # install solr:
 mkdir -p /tmp/solr || exit $?
 cd /tmp/solr || exit $?
-# wget "https://archive.apache.org/dist/lucene/solr/6.3.0/solr-6.3.0.zip"  || exit $?
-wget "http://apache.mirror.triple-it.nl/lucene/solr/5.5.4/solr-5.5.4.zip" || exit $?
-unzip solr-5.5.4.zip  || exit $?
+wget "http://archive.apache.org/dist/lucene/solr/${SOLR_VERSION}/solr-${SOLR_VERSION}.zip" || exit $?
+# wget "http://apache.mirror.triple-it.nl/lucene/solr/${SOLR_VERSION}/solr-${SOLR_VERSION}.zip" || exit $?
+unzip solr-${SOLR_VERSION}.zip  || exit $?
 mkdir /opt/solr  || exit $?
-cp -r /tmp/solr/solr-5.5.4/* /opt/solr  || exit $?
+cp -r /tmp/solr/solr-${SOLR_VERSION}/* /opt/solr  || exit $?
 mkdir -p /opt/solr/server/solr/openskos/conf  || exit $?
 touch /opt/solr/server/solr/openskos/core.properties || exit $?
 cp ${TRAVIS_BUILD_DIR}/data/solr/solrconfig.xml /opt/solr/server/solr/openskos/conf/solrconfig.xml || exit $?
@@ -17,8 +19,8 @@ cp ${TRAVIS_BUILD_DIR}/data/solr/schema.xml /opt/solr/server/solr/openskos/conf/
 chmod 755 ${TRAVIS_BUILD_DIR}/integrationtestsettings/start-solr.sh || exit $?
 
 # install fuseki:
-tar -zxvf ${TRAVIS_BUILD_DIR}/integrationtestsettings/apache-jena-fuseki-2.3.0.tar.gz -C /opt || exit $?
-mv /opt/apache-jena-fuseki-2.3.0 /opt/apache-jena-fuseki || exit $?
+tar -zxvf ${TRAVIS_BUILD_DIR}/integrationtestsettings/apache-jena-fuseki-${JENA_VERSION}.tar.gz -C /opt || exit $?
+mv /opt/apache-jena-fuseki-${JENA_VERSION} /opt/apache-jena-fuseki || exit $?
 chmod -R ugo+rw /opt/apache-jena-fuseki  || exit $?
 chmod +x /opt/apache-jena-fuseki/fuseki-server /opt/apache-jena-fuseki/bin/*  || exit $?
 mkdir -p /opt/apache-jena-fuseki/run  || exit $?
