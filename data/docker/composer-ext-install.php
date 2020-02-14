f977904667d4c691cced916ee5666c26a713bdd8
Robin Bron <finwo@pm.me>
2020-02-14 10:24:03 +0100

    Install dependencies by marking them as required in composer; not manually

--- data/docker/composer-ext-install.php
+++ data/docker/composer-ext-install.php
@@ -15,12 +15,13 @@ $app = json_decode(file_get_contents($argv[1]??(__DIR__.'/../composer.json')));
 $dep = json_decode(file_get_contents($argv[2]??(__DIR__.'/../composer.lock')));
 array_push($dep->packages, $app);
 
-foreach ($dep->packages as $package) {
-    $dependencies = (array) $package->require;
-    foreach ($dependencies as $name => $version) {
-        if ('ext-' !== substr($name, 0, 4)) {
-            continue;
-        }
+foreach($dep->packages as $package) {
+    $dependencies = array_merge(
+        (array) ($package->require ?? []),
+        (array) ($package->{'require-dev'} ?? [])
+    );
+    foreach($dependencies as $name => $version) {
+        if (substr($name, 0, 4) !== 'ext-') continue;
 
         if (isset($known[$name])) {
             continue;
@@ -29,15 +30,16 @@ foreach ($dep->packages as $package) {
         }
 
         $pipes = [];
-        $cmd   = sprintf($command, $prefix, substr($name, 4));
+        $cmd   = sprintf($command,$prefix,substr($name,4));
         echo ' ---> ', $cmd, PHP_EOL;
         $process = proc_open($cmd, $descriptors, $pipes, $cwd, $_ENV);
-        while (!feof($pipes[1])) {
-            printf('      '.fgets($pipes[1]));
+        while(!feof($pipes[1])) {
+            printf('      ' . fgets($pipes[1]));
         }
         echo PHP_EOL;
         fclose($pipes[0]);
         fclose($pipes[1]);
         proc_close($process);
+
     }
 }
