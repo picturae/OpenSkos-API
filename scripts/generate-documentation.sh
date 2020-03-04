f238a771abf24c84f113b583e7b605746aaea7c6
Robin Bron <finwo@pm.me>
2020-03-04 16:40:57 +0100

    Explicitly fail on non-commited documentation changes

--- scripts/generate-documentation.sh
+++ scripts/generate-documentation.sh
@@ -11,12 +11,32 @@ bindir=$(cd $(dirname $0) ; pwd)
 cd "${bindir}/.."
 approot=$(pwd)
 
-# Generate ontology & documentation
+# Error if the ontology changed
+echo " --> Generating ontology classes"
 php "${approot}/bin/console" ontology:generate
-php "${approot}/bin/console" swagger:generate
-php "${approot}/bin/console" exception:errorlist
+difflines=$(git diff config/ontology | wc -l)
+if (( $difflines > 0 )); then
+  echo "Changes were made to the ontology" >&2
+  echo "Please commit check and stage these first" >&2
+  exit 1
+fi
 
-# Add the updates to the commit
-git add config/ontology
-git add public/swagger.yaml
-git add src/Exception/list.json
+# Error if the documentation changed
+echo " --> Generating swagger documentation"
+php "${approot}/bin/console" swagger:generate
+difflines=$(git diff public/swagger.yaml | wc -l)
+if (( $difflines > 0 )); then
+  echo "Changes were made to public/swagger.yaml" >&2
+  echo "Please commit check and stage these first" >&2
+  exit 1
+fi
+
+# Error if the errors changed
+echo " --> Generating error documentation"
+php "${approot}/bin/console" exception:errorlist
+difflines=$(git diff src/Exception/list.json | wc -l)
+if (( $difflines > 0 )); then
+  echo "Changes were made to src/Exception/list.json" >&2
+  echo "Please commit check and stage these first" >&2
+  exit 1
+fi
